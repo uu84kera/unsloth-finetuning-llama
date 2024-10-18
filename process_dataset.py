a43e52a3d653177096b272841b7972d3af0b65d4
Lateim <112474675+uu84kera@users.noreply.github.com>
2024-10-18 18:32:25 +0800

    Update process_dataset.py

--- process_dataset.py
+++ process_dataset.py
@@ -17,14 +17,15 @@ def read_data(paths):
 
 if __name__ == "__main__":
     data_path = "cross_validation_datasets"
-    json_paths = glob("{}/val_fold_1.json".format(data_path))
+    json_paths = glob("{}/train_fold_1.json".format(data_path))
+    print(json_paths)
     questions, answers = read_data(json_paths)
     new_json_format = []
     for question, answer in zip(questions, answers):
         qa_pairs = {}
-        qa_pairs["instruction"] = question
-        qa_pairs["input"] = ""
+        qa_pairs["instruction"] = "请替我解答以下句子的真实意义，并且以尽可能简洁的语言回答."
+        qa_pairs["input"] = question
         qa_pairs["output"] = answer
         new_json_format.append(qa_pairs)
-    with open("ruozhiba_processed_val1.json", "w") as file:
+    with open("ruozhiba_processed_train1.json", "w") as file:
         json.dump(new_json_format, file)
